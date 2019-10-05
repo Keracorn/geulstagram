f35d915962cb7f2ca4dbc8e30475973316e6929b
Young Ahn <young_ahn@yonsei.ac.kr>
2019-10-05 16:58:40 +0900

    adding google colab code

---
+++ ggl_collab.py
@@ -0,0 +1,33 @@
+import os
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+from google.colab import auth
+from oauth2client.client import GoogleCredentials
+
+# 1. Authenticate and create the PyDrive client.
+auth.authenticate_user()
+gauth = GoogleAuth()
+gauth.credentials = GoogleCredentials.get_application_default()
+drive = GoogleDrive(gauth)
+
+# choose a local (colab) directory to store the data.
+local_download_path = os.path.expanduser('~/data')
+try:
+  os.makedirs(local_download_path)
+except: pass
+
+# 2. Auto-iterate using the query syntax
+#    https://developers.google.com/drive/v2/web/search-parameters
+file_list = drive.ListFile(
+    {'q': "'1SooKSw8M4ACbznKjnNrYvJ5wxuqJ-YCk' in parents"}).GetList()
+
+for f in file_list:
+  # 3. Create & download by id.
+  print('title: %s, id: %s' % (f['title'], f['id']))
+  fname = os.path.join(local_download_path, f['title'])
+  print('downloading to {}'.format(fname))
+  f_ = drive.CreateFile({'id': f['id']})
+  f_.GetContentFile(fname)
+
+with open(fname, 'r') as f:
+  print(f.read())
