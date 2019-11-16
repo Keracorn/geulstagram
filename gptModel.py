f550fd0e65e3ac0220234c9645da0640182db044
Hyunjin Bae <gloria9705@naver.com>
2019-11-16 14:45:56 +0900

    fixed code of gptModel.py

--- gptModel.py
+++ gptModel.py
@@ -3,11 +3,22 @@ import gpt_2_simple as gpt2
 
 def gpt(sentence):
     sess = gpt2.start_tf_sess()
+    print("sess")
     gpt2.load_gpt2(sess, run_name='run1')
-    gpt2.generate(sess)
+    print("load")
+    sent = gpt2.generate(sess, run_name='run1',
+              return_as_list=True,
+              include_prefix=False,
+              prefix=sentence,
+              truncate='<|endoftext|>')
+    print("generate")
+    gpt2.close()
+    print("close")
+
+    return sentence
 
 
-gpt("hi")
+
 
 
 
